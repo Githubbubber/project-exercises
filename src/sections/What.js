fa4f66355f24339bea0098c11b049310922f2779
Meke Brown <mekesiabrown@gmail.com>
2022-11-03 23:10:24 -0400

    refactor: break out li/link tags into DOM appended els

--- src/sections/What.js
+++ src/sections/What.js
@@ -6,33 +6,85 @@ const getTheWhatHeader = () => {
 };
 
 const getTheWhatSection = () => {
+    const pageDirectory = "./pages/";
+    let firstItem = 0, secondItem = 0;
+
     const theWhatSectionEl = document.createElement('section');
     const firstWhatPEl = document.createElement('p');
     const firstWhatTextNode = "You can find my revisiting the various FCC exercises with the following pages:";
     const firstWhatULEl = document.createElement('ul');
-    firstWhatULEl.innerHTML = `<li><a href="./pages/cats.html">A Page Dedicated to Cats</a></li>
-<li><a href="./pages/cats2.html">Another Page Dedicated to Cats</a></li>
-<li><a href="./pages/coffee-shop.html">Camper Cafe</a></li>
-<li><a href="./pages/markers.html">CSS Color Markers</a></li>
-<li><a href="./pages/form.html">A pretty Html Form</a></li>
-<li><a href="./pages/rothko.html">Rothko Painting Creation</a></li>
-<li><a href="./pages/nutrition-label.html">US Nutrition Label</a></li>
-<li><a href="./pages/accessible-quiz.html">Accessible Quiz</a></li>
-<li><a href="./pages/balance-sheet.html">A Balance Sheet</a></li>
-<li><a href="./pages/picasso.html">A Picasso-Inspired Painting</a></li>
-<li><a href="./pages/piano.html">freeCodeCamp Piano</a></li>
-<li><a href="./pages/city.html">City... Resize to see day to night</a></li>
-<li><a href="./pages/magazine.html">Bored? Read a freeCodeCamp Magazine</a></li>
-<li><a href="./pages/ferris-wheel.html">A Fun Ferris Wheel Animation</a></li>
-<li><a href="./pages/penguin.html">Penguin Animation</a></li>
-<li><a href="./pages/admin-dashboard2.html">Admin Dashboard</a> and <a href="./pages/admin-dashboard.html"></a>the first attempt</li>`;
+
+    const firstWhatNames = [
+        ["cats.html", "A Page Dedicated to Cats"],
+        ["cats2.html", "Another Page Dedicated to Cats"],
+        ["coffee-shop.html", "Camper Cafe"],
+        ["markers.html", "CSS Color Markers"],
+        ["form.html", "A pretty HTML Form"],
+        ["rothko.html", "Rothko Painting Creation"],
+        ["nutrition-label.html", "US Nutrition Label"],
+        ["accessible-quiz.html", "Accessible Quiz"],
+        ["balance-sheet.html", "A Balance Sheet"],
+        ["picasso.html", "A Picasso-Inspired Painting"],
+        ["piano.html", "freeCodeCamp Piano"],
+        ["city.html", "City... Resize to see day to night"],
+        ["magazine.html", "Bored? Read a freeCodeCamp Magazine"],
+        ["ferris-wheel.html", "A Fun Ferris Wheel Animation"],
+        ["penguin.html", "Penguin Animation"],
+    ];
+
+    const firstWhatNamesLen = firstWhatNames.length;
+
+    do {
+        const firstWhatLi = document.createElement('li');
+        const firstWhatText = document.createTextNode(" | ");
+        const firstWhatA = document.createElement('a');
+        firstWhatA.setAttribute('href', pageDirectory + firstWhatNames[firstItem][0]);
+        firstWhatA.textContent = firstWhatNames[firstItem][1];
+
+        firstWhatLi.appendChild(firstWhatA);
+        firstWhatLi.appendChild(firstWhatText);
+
+        firstWhatULEl.appendChild(firstWhatLi);
+
+        firstItem++;
+    } while (firstItem < firstWhatNamesLen);
+
+    const lastFirstWhatLi = document.createElement('li');
+    lastFirstWhatLi.innerHTML = "<a href='./pages/admin-dashboard2.html'>Admin Dashboard</a> and <a href='./pages/admin-dashboard.html'>the first attempt</a>";
+
+    firstWhatULEl.appendChild(lastFirstWhatLi);
+
+    const secondWhatNames = [
+        ["example.html", "Generic Example Page"],
+        ["calculator.html", "Calculator"],
+        ["tictactoe.html", "Tic Tac Toe"],
+    ];
+
+    const secondWhatNamesLen = secondWhatNames.length;
+
     const secondWhatPEl = document.createElement('p');
     const secondWhatTextNode = "You can find my work for exercises from The Odin Project here:";
     const secondWhatULEl = document.createElement('ul');
-    secondWhatULEl.innerHTML = `<li><a href="./pages/example.html">Generic Example Page</a> | </li>
-    <li><a href="./pages/calculator.html">Calculator</a> | </li>
-    <li><a href="./pages/tictactoe.html">Tic Tac Toe</a> | </li>
-    <li><a href="./pages/restaurant.html">A Restaurant Page</a></li>`;
+
+    do {
+        const secondWhatLi = document.createElement('li');
+        const secondWhatText = document.createTextNode(" | ");
+        const secondWhatA = document.createElement('a');
+        secondWhatA.setAttribute('href', pageDirectory + secondWhatNames[secondItem][0]);
+        secondWhatA.textContent = secondWhatNames[secondItem][1];
+
+        secondWhatLi.appendChild(secondWhatA);
+        secondWhatLi.appendChild(secondWhatText);
+
+        secondWhatULEl.appendChild(secondWhatLi);
+
+        secondItem++;
+    } while (secondItem < secondWhatNamesLen);
+
+    const lastSecondWhatLi = document.createElement('li');
+    lastSecondWhatLi.innerHTML = "<a href='./pages/restaurant.html'>A Restaurant Page</a>";
+
+    secondWhatULEl.appendChild(lastSecondWhatLi);
 
     firstWhatPEl.appendChild(document.createTextNode(firstWhatTextNode));
     firstWhatPEl.appendChild(firstWhatULEl);
