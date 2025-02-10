68a0c09d3c46664ddaad1cf63d6b91e94b50c9aa
Mekesia Brown <mekesiabrown@gmail.com>
2025-02-10 10:27:50 -0500

    feat: update with todo/project localStorage code

--- dist/js/todo/index.js
+++ dist/js/todo/index.js
@@ -1,3 +1,21 @@
+import { StorageManager } from "./components/Storage";
+import Item from "/src/exercises/todo/TodoItemClass";
+
+const storageManager = new StorageManager();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const storedProjects = storageManager.getStoredProjects();
+
+    // Render out the list of project titles and other info
+    return storedProjects;
+});
+
+document.getElementById("get-project-todos").addEventListener("click", (e) => {
+    const storedProject = storageManager.getStoredProjects(e.target.value);
+    
+    return storedProject.getTodos();
+});
+
 document.getElementById("add-todo").addEventListener("click", () => {
     const title = document.getElementById("todo-title").value;
     const priority = document.getElementById("todo-priority").value;
