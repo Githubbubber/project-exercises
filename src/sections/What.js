083da0c2b1c49e6bcd0fb37794ed6aeb822ff0f3
Mekesia Brown <mekesiabrown@gmail.com>
2025-02-15 19:02:27 -0500

    refactor: handle html file names and location

--- src/sections/What.js
+++ src/sections/What.js
@@ -1,3 +1,5 @@
+import { firstWhatNames } from "../content/assets/constants";
+
 const getTheWhatHeader = () => {
     const h2WhatEl = document.createElement('h2');
     h2WhatEl.textContent = "The what...";
@@ -6,7 +8,7 @@ const getTheWhatHeader = () => {
 };
 
 const getTheWhatSection = () => {
-    const pageDirectory = "./pages/";
+    const pageDirectory = "./content/pages/";
     let firstItem = 0, secondItem = 0;
 
     const theWhatSectionEl = document.createElement('section');
@@ -14,31 +16,13 @@ const getTheWhatSection = () => {
     const firstWhatTextNode = "You can find my revisiting the various FCC projects with the following pages:";
     const firstWhatULEl = document.createElement('ul');
 
-    const firstWhatNames = [
-        ["cats.html", "A Page Dedicated to Cats"],
-        ["cats2.html", "Another Page Dedicated to Cats"],
-        ["coffee-shop.html", "Camper Cafe"],
-        ["markers.html", "CSS Color Markers"],
-        ["form.html", "A pretty HTML Form"],
-        ["rothko.html", "Rothko Painting Creation"],
-        ["nutrition-label.html", "US Nutrition Label"],
-        ["accessible-quiz.html", "Accessible Quiz"],
-        ["balance-sheet.html", "A Balance Sheet"],
-        ["picasso.html", "A Picasso-Inspired Painting"],
-        ["piano.html", "freeCodeCamp Piano"],
-        ["city.html", "City... Resize to see day to night"],
-        ["magazine.html", "Bored? Read a freeCodeCamp Magazine"],
-        ["ferris-wheel.html", "A Fun Ferris Wheel Animation"],
-        ["penguin.html", "Penguin Animation"],
-    ];
-
     const firstWhatNamesLen = firstWhatNames.length;
 
     do {
         const firstWhatLi = document.createElement('li');
         const firstWhatText = document.createTextNode(" | ");
         const firstWhatA = document.createElement('a');
-        firstWhatA.setAttribute('href', pageDirectory + firstWhatNames[firstItem][0]);
+        firstWhatA.setAttribute('href', firstWhatNames[firstItem][0]);
         firstWhatA.textContent = firstWhatNames[firstItem][1];
 
         firstWhatLi.appendChild(firstWhatA);
@@ -58,6 +42,7 @@ const getTheWhatSection = () => {
         ["example.html", "Generic Example Page"],
         ["calculator.html", "Calculator"],
         ["tictactoe.html", "Tic Tac Toe"],
+        ["todo.html", "A Todo List"],
     ];
 
     const secondWhatNamesLen = secondWhatNames.length;
@@ -70,7 +55,7 @@ const getTheWhatSection = () => {
         const secondWhatLi = document.createElement('li');
         const secondWhatText = document.createTextNode(" | ");
         const secondWhatA = document.createElement('a');
-        secondWhatA.setAttribute('href', pageDirectory + secondWhatNames[secondItem][0]);
+        secondWhatA.setAttribute('href', secondWhatNames[secondItem][0]);
         secondWhatA.textContent = secondWhatNames[secondItem][1];
 
         secondWhatLi.appendChild(secondWhatA);
