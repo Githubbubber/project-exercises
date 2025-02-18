228ef01448773b81e61ffdb0ceecdcbb40a78989
Mekesia Brown <mekesiabrown@gmail.com>
2025-02-17 20:55:38 -0500

    refactor: add heading nav

--- src/sections/Heading.js
+++ src/sections/Heading.js
@@ -1,9 +1,30 @@
+import { firstWhatNames } from "../content/assets/js/etc/constants";
+
 const getHeading = () => {
-    const h1El = document.createElement('h1');
+    const headingEl = document.createElement('heading');
+    const h1El = document.createElement("h1");
+    const navEl = document.createElement("nav");
+    const ulEl = document.createElement("ul");
 
-    h1El.textContent = "Welcome!";
+    h1El.textContent = "Here are the pages for my FCC and TOP work:";
 
-    return h1El;
+    firstWhatNames.forEach(page => {
+        const liEl = document.createElement("li");
+        const aEl = document.createElement("a");
+
+        aEl.setAttribute("href", page[0]);
+        aEl.textContent = page[1];
+
+        liEl.appendChild(aEl);
+
+        ulEl.appendChild(liEl);
+    });
+
+    navEl.appendChild(ulEl)
+    headingEl.appendChild(h1El);
+    headingEl.appendChild(navEl);
+
+    return headingEl;
 };
 
 export default getHeading;
